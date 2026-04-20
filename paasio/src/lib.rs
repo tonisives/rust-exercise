b8f7aad96bae7f45e8c8b61be016dcf2ee4081e9
tonisives <tonisives@gmail.com>
2026-04-21 06:59:44 +0700

    implement stub

--- paasio/src/lib.rs
+++ paasio/src/lib.rs
@@ -3,26 +3,34 @@ use std::io::{Read, Result, Write};
 // the PhantomData instances in this file are just to stop compiler complaints
 // about missing generics; feel free to remove them
 
-pub struct ReadStats<R>(::std::marker::PhantomData<R>);
+pub struct ReadStats<R> {
+    wrapped: R,
+    bytes_through: usize,
+    reads: usize,
+}
 
 impl<R: Read> ReadStats<R> {
     // _wrapped is ignored because R is not bounded on Debug or Display and therefore
     // can't be passed through format!(). For actual implementation you will likely
     // wish to remove the leading underscore so the variable is not ignored.
-    pub fn new(_wrapped: R) -> ReadStats<R> {
-        todo!()
+    pub fn new(wrapped: R) -> ReadStats<R> {
+        ReadStats {
+            wrapped,
+            bytes_through: 0,
+            reads: 0,
+        }
     }
 
     pub fn get_ref(&self) -> &R {
-        todo!()
+        &self.wrapped
     }
 
     pub fn bytes_through(&self) -> usize {
-        todo!()
+        self.bytes_through
     }
 
     pub fn reads(&self) -> usize {
-        todo!()
+        self.reads
     }
 }
 
@@ -32,26 +40,34 @@ impl<R: Read> Read for ReadStats<R> {
     }
 }
 
-pub struct WriteStats<W>(::std::marker::PhantomData<W>);
+pub struct WriteStats<W> {
+    wrapped: W,
+    bytes_through: usize,
+    writes: usize,
+}
 
 impl<W: Write> WriteStats<W> {
     // _wrapped is ignored because W is not bounded on Debug or Display and therefore
     // can't be passed through format!(). For actual implementation you will likely
     // wish to remove the leading underscore so the variable is not ignored.
-    pub fn new(_wrapped: W) -> WriteStats<W> {
-        todo!()
+    pub fn new(wrapped: W) -> WriteStats<W> {
+        WriteStats {
+            wrapped,
+            bytes_through: 0,
+            writes: 0,
+        }
     }
 
     pub fn get_ref(&self) -> &W {
-        todo!()
+        &self.wrapped
     }
 
     pub fn bytes_through(&self) -> usize {
-        todo!()
+        self.bytes_through
     }
 
     pub fn writes(&self) -> usize {
-        todo!()
+        self.writes
     }
 }
 
