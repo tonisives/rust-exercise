3c6dda309c707eba6501b27f1ddb41044d4b0e31
tonisives <tonisives@gmail.com>
2026-04-22 07:15:22 +0700

    update

--- binary-search/src/lib.rs
+++ binary-search/src/lib.rs
@@ -5,7 +5,10 @@ pub fn find(array: &[i32], key: i32) -> Option<usize> {
     // divide and conquer. binary search: a sorted list is split in the middle, and then either
     // goes toward left or right again in the middle and comparing the new number until it finds
     // the target
+    find_offset(array, key, 0)
+}
 
+pub fn find_offset(array: &[i32], key: i32, offset: u32) -> Option<usize> {
     if array.len() == 0 {
         return None;
     }
@@ -13,15 +16,13 @@ pub fn find(array: &[i32], key: i32) -> Option<usize> {
     let mid = array.len() / 2;
     let mid_val = array[mid];
 
-    if mid_val == key {
-        return Some(mid);
+    return if mid_val == key {
+        Some(mid + offset as usize)
     } else if key > mid_val {
         let right = &array[mid + 1..];
-        find(right, (right.len() / 2) as i32);
+        find_offset(right, key, offset + mid as u32 + 1)
     } else {
         let left = &array[0..mid];
-        find(left, (left.len() / 2) as i32);
-    }
-
-    None
+        find_offset(left, key, offset)
+    };
 }
