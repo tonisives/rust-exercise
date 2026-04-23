a8d4527085c531a00585b7f9f60c0a373b13bae1
tonisives <tonisives@gmail.com>
2026-04-24 05:36:23 +0700

    update

--- parallel-letter-frequency/src/lib.rs
+++ parallel-letter-frequency/src/lib.rs
@@ -29,15 +29,20 @@ pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
         .into_iter()
         .for_each(|handle| {
             if let Ok(map) = handle.join() {
-                map.iter()
-                    .for_each(|(char, size)| {
-                        *res.entry(*char)
-                            .or_insert(0) += size
-                    })
+                add_to_map(map, &mut res);
             }
         });
 
     res
+}
+
+fn add_to_map(from: HashMap<char, usize>, into: &mut HashMap<char, usize>) {
+    from.iter()
+        .for_each(|(char, size)| {
+            *into
+                .entry(*char)
+                .or_insert(0) += size
+        })
 }
 
 fn spawn_counter(input: &[&str]) -> JoinHandle<HashMap<char, usize>> {
@@ -50,16 +55,18 @@ fn spawn_counter(input: &[&str]) -> JoinHandle<HashMap<char, usize>> {
         let mut res: HashMap<char, usize> = HashMap::new();
 
         copy.iter()
-            .for_each(|str| {
-                str.chars()
-                    .filter(|c| c.is_alphabetic())
-                    .flat_map(|c| c.to_lowercase())
-                    .for_each(|char| {
-                        *res.entry(char)
-                            .or_insert(0) += 1;
-                    })
-            });
+            .for_each(|str| add_chars(str, &mut res));
 
         res
     })
+}
+
+fn add_chars(str: &str, map: &mut HashMap<char, usize>) {
+    str.chars()
+        .filter(|c| c.is_alphabetic())
+        .flat_map(|c| c.to_lowercase())
+        .for_each(|char| {
+            *map.entry(char)
+                .or_insert(0) += 1;
+        })
 }
