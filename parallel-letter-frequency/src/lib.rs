3a4a4cb5e8d04747d185ae12a535145a58f9f438
tonisives <tonisives@gmail.com>
2026-04-23 08:16:12 +0700

    update

--- parallel-letter-frequency/src/lib.rs
+++ parallel-letter-frequency/src/lib.rs
@@ -14,33 +14,43 @@ pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
     let mut res: HashMap<char, usize> = HashMap::new();
     let chunks = input.chunks(worker_count);
 
-    // we need
-    // worker count threads
-    // each processes a slice from input
-    // returns char count map for the slice
-    // in the end, merge the slices
-    let handles: Vec<_> = chunks.map(|chunk| spawn_counter(chunk).join()).collect();
-    handles.iter().for_each(|handle| match handle {
-        Ok(handle_ok) => handle_ok
-            .iter()
-            .for_each(|(char, size)| *res.entry(*char).or_insert(0) += size),
-        Err(_) => (),
-    });
+    let handles: Vec<_> = chunks
+        .map(|chunk| spawn_counter(chunk).join())
+        .collect();
+    handles
+        .iter()
+        .for_each(|handle| match handle {
+            Ok(handle_ok) => handle_ok
+                .iter()
+                .for_each(|(char, size)| {
+                    *res.entry(*char)
+                        .or_insert(0) += size
+                }),
+            Err(_) => (),
+        });
 
     res
 }
 
 fn spawn_counter(input: &[&str]) -> JoinHandle<HashMap<char, usize>> {
-    let copy: Vec<String> = input.iter().map(|c| c.to_string()).collect();
+    let copy: Vec<String> = input
+        .iter()
+        .map(|c| c.to_string())
+        .collect();
 
     spawn(move || {
         let mut res: HashMap<char, usize> = HashMap::new();
 
-        copy.iter().for_each(|str| {
-            str.chars().for_each(|char| {
-                *res.entry(char).or_insert(0) += 1;
-            })
-        });
+        copy.iter()
+            .for_each(|str| {
+                str.chars()
+                    .filter(|c| c.is_alphabetic())
+                    .flat_map(|c| c.to_lowercase())
+                    .for_each(|char| {
+                        *res.entry(char)
+                            .or_insert(0) += 1;
+                    })
+            });
 
         res
     })
--- parallel-letter-frequency/tests/parallel_letter_frequency.rs
+++ parallel-letter-frequency/tests/parallel_letter_frequency.rs
@@ -8,7 +8,7 @@ fn no_texts() {
 }
 
 #[test]
-#[ignore]
+
 fn one_text_with_one_letter() {
     let mut hm = HashMap::new();
     hm.insert('a', 1);
@@ -16,7 +16,7 @@ fn one_text_with_one_letter() {
 }
 
 #[test]
-#[ignore]
+
 fn one_text_with_multiple_letters() {
     let mut hm = HashMap::new();
     hm.insert('b', 2);
@@ -26,7 +26,7 @@ fn one_text_with_multiple_letters() {
 }
 
 #[test]
-#[ignore]
+
 fn two_texts_with_one_letter() {
     let mut hm = HashMap::new();
     hm.insert('e', 1);
@@ -35,7 +35,7 @@ fn two_texts_with_one_letter() {
 }
 
 #[test]
-#[ignore]
+
 fn two_texts_with_multiple_letters() {
     let mut hm = HashMap::new();
     hm.insert('g', 2);
@@ -45,7 +45,7 @@ fn two_texts_with_multiple_letters() {
 }
 
 #[test]
-#[ignore]
+
 fn case_insensitivity() {
     let mut hm = HashMap::new();
     hm.insert('a', 2);
@@ -53,21 +53,21 @@ fn case_insensitivity() {
 }
 
 #[test]
-#[ignore]
+
 fn many_empty_lines() {
     let v = vec![""; 1000];
     assert_eq!(frequency::frequency(&v[..], 4), HashMap::new());
 }
 
 #[test]
-#[ignore]
+
 fn ignore_whitespace() {
     let v = ["   ", "\t", "\r\n"];
     assert_eq!(frequency::frequency(&v[..], 4), HashMap::new());
 }
 
 #[test]
-#[ignore]
+
 fn many_times_same_text() {
     let v = vec!["abc"; 1000];
     let mut hm = HashMap::new();
@@ -78,19 +78,19 @@ fn many_times_same_text() {
 }
 
 #[test]
-#[ignore]
+
 fn punctuation_doesnt_count() {
     assert!(!frequency::frequency(&WILHELMUS, 4).contains_key(&','));
 }
 
 #[test]
-#[ignore]
+
 fn numbers_dont_count() {
     assert!(!frequency::frequency(&["Testing, 1, 2, 3"], 4).contains_key(&'1'));
 }
 
 #[test]
-#[ignore]
+
 fn unicode_letters() {
     let mut hm = HashMap::new();
     hm.insert('本', 1);
@@ -102,7 +102,7 @@ fn unicode_letters() {
 }
 
 #[test]
-#[ignore]
+
 fn all_three_anthems_1_worker() {
     let mut v = Vec::new();
     // These constants can be found under the last test if you wish to see them
@@ -118,7 +118,7 @@ fn all_three_anthems_1_worker() {
 }
 
 #[test]
-#[ignore]
+
 fn all_three_anthems_3_workers() {
     let mut v = Vec::new();
     for anthem in [ODE_AN_DIE_FREUDE, WILHELMUS, STAR_SPANGLED_BANNER].iter() {
@@ -133,7 +133,7 @@ fn all_three_anthems_3_workers() {
 }
 
 #[test]
-#[ignore]
+
 fn non_integer_multiple_of_threads() {
     let v = vec!["abc"; 999];
     let mut hm = HashMap::new();
@@ -144,7 +144,7 @@ fn non_integer_multiple_of_threads() {
 }
 
 #[test]
-#[ignore]
+
 fn large_texts() {
     let expected: HashMap<char, usize> = [
         ('a', 845),
