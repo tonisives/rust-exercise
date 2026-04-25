110cbbe196cbbfc003a53f6b8e2bd2d1ba4726ce
tonisives <tonisives@gmail.com>
2026-04-25 12:42:29 +0700

    update

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get, serve};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(root_handler));
+    let port = 3000;
+
+    let listener = TcpListener::bind(format!("127.0.0.1:{}", port))
+        .await
+        .unwrap();
+
+    println!("server running on {}", port);
+
+    serve(listener, app).await.unwrap();
+}
+
+async fn root_handler() -> &'static str {
+    "Hello from Axum!"
 }
