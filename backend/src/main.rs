fe57bb185571be6bb2509a9c96cd99be0fd24d7f
tonisives <tonisives@gmail.com>
2026-04-25 12:53:40 +0700

    update

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,11 +1,14 @@
 use axum::{Router, routing::get, serve};
+use sqlx::PgPool;
 use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
     let app = Router::new()
         .route("/", get(root_handler))
-        .route("/health", get(health));
+        .route("/health", get(health))
+        .route("/pools", get(pools));
+
     let port = 3000;
 
     let listener = TcpListener::bind(format!("127.0.0.1:{}", port))
@@ -15,6 +18,19 @@ async fn main() {
     println!("server running on {}", port);
 
     serve(listener, app).await.unwrap();
+}
+
+struct PoolJson {
+    name: String,
+    apy: f32,
+}
+
+struct AppState {
+    db: PgPool,
+}
+
+async fn pools() -> &'static str {
+    "Hello from Axum!"
 }
 
 async fn root_handler() -> &'static str {
