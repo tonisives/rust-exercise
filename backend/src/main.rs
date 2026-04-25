2f31e84091f5a922c8e371e6d0fc59493b5c458b
tonisives <tonisives@gmail.com>
2026-04-25 12:59:05 +0700

    update

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,9 +1,28 @@
+use std::env;
+
 use axum::{Router, routing::get, serve};
-use sqlx::PgPool;
+use sqlx::{PgPool, postgres::PgPoolOptions};
 use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
+    let env = Env {
+        DATABASE_URL: env::var("DATABASE_URL").unwrap(),
+    };
+
+    let state = AppState {
+        db: PgPoolOptions::new()
+            .max_connections(10)
+            .min_connections(1)
+            .acquire_timeout(std::time::Duration::from_secs(5))
+            .idle_timeout(std::time::Duration::from_secs(300))
+            .max_lifetime(std::time::Duration::from_secs(1800))
+            .test_before_acquire(true)
+            .connect_lazy(&env.DATABASE_URL)
+            .unwrap(),
+        env,
+    };
+
     let app = Router::new()
         .route("/", get(root_handler))
         .route("/health", get(health))
@@ -25,8 +44,14 @@ struct PoolJson {
     apy: f32,
 }
 
+#[allow(non_snake_case)]
+struct Env {
+    DATABASE_URL: String,
+}
+
 struct AppState {
     db: PgPool,
+    env: Env,
 }
 
 async fn pools() -> &'static str {
@@ -34,7 +59,7 @@ async fn pools() -> &'static str {
 }
 
 async fn root_handler() -> &'static str {
-    "Hello from Axum!"
+    "{\"msg\":\"working\"}"
 }
 
 async fn health() -> &'static str {
