e03fc2684c68ef058cd5e0d5a7e8431a8c99c7ac
tonisives <tonisives@gmail.com>
2026-06-09 09:33:23 +0700

    update

--- flower-field/src/lib.rs
+++ flower-field/src/lib.rs
@@ -1,5 +1,32 @@
+// each empty square should show the number of mines nearby
 pub fn annotate(garden: &[&str]) -> Vec<String> {
-    todo!(
-        "\nAnnotate each square of the given garden with the number of flowers that surround said square (blank if there are no surrounding flowers):\n{garden:#?}\n"
-    );
+    println!("{garden:#?}");
+
+    // solution 1: loop every character, if is not a mine, then loop all nearby nodes, and sum the
+    // answer to the current position
+    let mut res: Vec<String> = vec![]; // TOD: use arr
+
+    for i in 0..garden.len() {
+        let row = garden[i].as_bytes();
+        let mut mapped_row = vec![0u8; row.len()];
+
+        for j in 0..row.len() {
+            if row[j] == b'*' {
+                mapped_row[j] = b'*'
+            } else {
+                mapped_row[j] = find_nearby_count(i, j, garden)
+            }
+        }
+
+        res.push(String::from_utf8(mapped_row).expect("not valid utf8"));
+    }
+
+    res
+}
+
+fn find_nearby_count(i: usize, j: usize, garden: &[&str]) -> u8 {
+    // don't go left/top/right/bottom if on the edge. otherwise, loop neighbours and count the mines
+    //
+
+    b'*'
 }
--- flower-field/tests/flower_field.rs
+++ flower-field/tests/flower_field.rs
@@ -9,7 +9,7 @@ fn no_rows() {
 }
 
 #[test]
-#[ignore]
+
 fn no_columns() {
     let input = &[""];
     let expected = &[""];
@@ -18,7 +18,7 @@ fn no_columns() {
 }
 
 #[test]
-#[ignore]
+
 fn no_flowers() {
     #[rustfmt::skip]
     let (input, expected) = (&[
@@ -35,7 +35,7 @@ fn no_flowers() {
 }
 
 #[test]
-#[ignore]
+
 fn garden_full_of_flowers() {
     #[rustfmt::skip]
     let (input, expected) = (&[
@@ -52,7 +52,7 @@ fn garden_full_of_flowers() {
 }
 
 #[test]
-#[ignore]
+
 fn flower_surrounded_by_spaces() {
     #[rustfmt::skip]
     let (input, expected) = (&[
@@ -69,7 +69,7 @@ fn flower_surrounded_by_spaces() {
 }
 
 #[test]
-#[ignore]
+
 fn space_surrounded_by_flowers() {
     #[rustfmt::skip]
     let (input, expected) = (&[
@@ -86,7 +86,7 @@ fn space_surrounded_by_flowers() {
 }
 
 #[test]
-#[ignore]
+
 fn horizontal_line() {
     let input = &[" * * "];
     let expected = &["1*2*1"];
@@ -95,7 +95,7 @@ fn horizontal_line() {
 }
 
 #[test]
-#[ignore]
+
 fn horizontal_line_flowers_at_edges() {
     let input = &["*   *"];
     let expected = &["*1 1*"];
@@ -104,7 +104,7 @@ fn horizontal_line_flowers_at_edges() {
 }
 
 #[test]
-#[ignore]
+
 fn vertical_line() {
     #[rustfmt::skip]
     let (input, expected) = (&[
@@ -125,7 +125,7 @@ fn vertical_line() {
 }
 
 #[test]
-#[ignore]
+
 fn vertical_line_flowers_at_edges() {
     #[rustfmt::skip]
     let (input, expected) = (&[
@@ -146,7 +146,7 @@ fn vertical_line_flowers_at_edges() {
 }
 
 #[test]
-#[ignore]
+
 fn cross() {
     #[rustfmt::skip]
     let (input, expected) = (&[
@@ -167,7 +167,7 @@ fn cross() {
 }
 
 #[test]
-#[ignore]
+
 fn large_garden() {
     #[rustfmt::skip]
     let (input, expected) = (&[
@@ -190,7 +190,7 @@ fn large_garden() {
 }
 
 #[test]
-#[ignore]
+
 fn multiple_adjacent_flowers() {
     let input = &[" ** "];
     let expected = &["1**1"];
