0424ec5470b3f20c2d094e3b1fa298416005d4df
tonisives <tonisives@gmail.com>
2026-06-16 07:30:57 +0700

    "remove superf"

--- easy/nth-prime/src/lib.rs
+++ easy/nth-prime/src/lib.rs
@@ -23,14 +23,8 @@ fn is_prime(n: u32) -> bool {
     if n <= 1 {
         return false;
     }
-    if n <= 3 {
-        return true;
-    }
-    if n % 2 == 0 || n % 3 == 0 {
-        return false;
-    }
 
-    let mut i = 5;
+    let mut i = 2;
     while i * i <= n {
         if n % i == 0 {
             return false;
