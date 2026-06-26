ff05b8ea87944e7710aecd57f416df69f1bee9b3
tonisives <tonisives@gmail.com>
2026-06-26 12:54:10 +0700

    update

--- med/acronym/src/lib.rs
+++ med/acronym/src/lib.rs
@@ -1,6 +1,7 @@
 pub fn abbreviate(phrase: &str) -> String {
     let mut acronym: Vec<char> = vec![];
     let mut prev_whitespace = true;
+    let mut prev_lowercase = false;
 
     for c in phrase.chars() {
         if c == ' ' || c == '-' {
@@ -9,8 +10,17 @@ pub fn abbreviate(phrase: &str) -> String {
             if prev_whitespace {
                 acronym.push(c.to_ascii_uppercase());
                 prev_whitespace = false;
-            } else if c.is_uppercase() {
-                acronym.push(c)
+                prev_lowercase = false;
+                continue;
+            }
+
+            if c.is_uppercase() {
+                if prev_lowercase {
+                    acronym.push(c);
+                }
+                prev_lowercase = false
+            } else if c.is_lowercase() {
+                prev_lowercase = true;
             }
         }
     }
